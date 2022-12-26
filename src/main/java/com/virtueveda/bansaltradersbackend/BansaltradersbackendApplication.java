a014bbff15683324567bfbbb29ebaddf56aa1dd7
divinedryfruitsandfoods <rushikeshkarkhanis@Rushikeshs-MacBook-Pro.local>
2022-12-26 15:51:21 +0530

    First Commit

---
+++ src/main/java/com/virtueveda/bansaltradersbackend/BansaltradersbackendApplication.java
@@ -0,0 +1,13 @@
+package com.virtueveda.bansaltradersbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BansaltradersbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BansaltradersbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/virtueveda/bansaltradersbackend/BansaltradersbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtueveda.bansaltradersbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BansaltradersbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
