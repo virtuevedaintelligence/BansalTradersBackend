192ed2bb1dbe71ae2c55b176fc83c9f12cb683c9
Karkhanis <rushikeshkarkhanis@Rushikeshs-MacBook-Pro.local>
2023-02-07 22:50:12 +0530

    User Dummy api

--- src/main/java/com/vvi/btb/constant/UserImplConstant.java
+++ src/main/java/com/vvi/btb/constant/UserImplConstant.java
@@ -4,4 +4,6 @@ import java.util.function.Supplier;
 
 public class UserImplConstant {
     public static Supplier USER_NOT_FOUND = () -> UserImplConstant.USER_NOT_FOUND;
+    public static String USER_OTP_GENERATED = "OTP Generated Successfully";
+    public static String USER_NOT_OTP_GENERATED = "OTP Generation Failed";
 }
--- src/main/java/com/vvi/btb/controller/UserResource.java
+++ src/main/java/com/vvi/btb/controller/UserResource.java
@@ -1,4 +1,33 @@
 package com.vvi.btb.controller;
 
+import com.vvi.btb.constant.UserImplConstant;
+import com.vvi.btb.domain.HttpResponse;
+import com.vvi.btb.util.Response;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.BAD_GATEWAY;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/v1/users")
+@Slf4j
 public class UserResource {
+
+    private final Response response;
+
+    public UserResource(Response response) {
+        this.response = response;
+    }
+
+    @GetMapping("/generateOTP/{number}")
+    public ResponseEntity<HttpResponse> createProduct(@PathVariable("number") String number)
+    {
+        if(number.equalsIgnoreCase("999999999")){
+            return response.response(HttpStatus.OK, UserImplConstant.USER_OTP_GENERATED, null);
+        }
+        return response.response(BAD_GATEWAY, UserImplConstant.USER_NOT_OTP_GENERATED,null);
+    }
 }
